333f3d899b14677bf1145b69d835b6ca302558e6
Luis Felipe <luis.felipeszsv@gmail.com>
2025-03-12 22:16:08 -0300

    feat: add console . log for testing

--- src/queries/expenses.ts
+++ src/queries/expenses.ts
@@ -7,6 +7,7 @@ export const expenseOptions = queryOptions({
   queryKey: [getExpensesQueryKey],
   queryFn: async () => {
     const response = await api.get("/expenses");
+    console.log(response);
     return response.data;
   },
   retry: 2,
